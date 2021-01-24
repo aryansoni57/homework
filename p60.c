a94308fcfaf8b8160cc4657164e84b6d49ff3a7e
Amitonagasaki <57029788+Amitonagasaki@users.noreply.github.com>
2021-01-25 03:59:10 +0530

    Create p60.c

---
+++ p60.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+    char a;
+    printf(" enter the character = ");
+    scanf(" %c", &a);
+    if (a =='r')
+    {
+        printf("red");
+    }
+     if (a =='p')
+    {
+        printf("pink");
+    }
+     if (a =='y')
+    {
+        printf("yellow");
+    }
+return 0;
+}
